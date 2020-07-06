087578624bc372e07ffc188ff2ed3e9bd25bb930
qinlong.shi <qinlong.shi@sap.com>
2020-07-06 22:33:33 +0800

    init spring boot project

---
+++ src/test/java/com/shiqla/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,15 @@
+package com.shiqla.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests
+{
+
+    @Test
+    void contextLoads ()
+    {
+    }
+
+}
