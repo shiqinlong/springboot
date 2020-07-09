0a3001dc0172cd118527e5a2e09f36d1c917cc37
qinlong.shi <qinlong.shi@sap.com>
2020-07-09 17:18:28 +0800

    update

---
+++ src/main/java/com/shiqla/springboot/autoconfiguration/AutoConfigContinter.java
@@ -0,0 +1,14 @@
+package com.shiqla.springboot.autoconfiguration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Desc
+ * Auth c5285333
+ * Date 2020-07-08
+ */
+@Configuration
+public class AutoConfigContinter
+{
+
+}
